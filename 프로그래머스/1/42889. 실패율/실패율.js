94605f0a5880f840ed5bbc178cf83dbdc1fb64a7
KimSoohyun <tina971007@naver.com>
2025-01-14 00:30:49 +0900

    [level 1] Title: 실패율, Time: 1393.76 ms, Memory: 42 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -0,0 +1,15 @@
+const solution = (N, stages) => {
+  const stageFailed = [];
+  let playersRemaining = stages.length;
+
+  for (let i = 1; i <= N; i++) {
+    const currentStagePlayers = stages.filter(stage => stage === i).length;
+    const failureRate = playersRemaining === 0 ? 0 : currentStagePlayers / playersRemaining;
+    stageFailed.push({ stage: i, failureRate });
+    playersRemaining -= currentStagePlayers;
+  }
+
+  return stageFailed
+    .sort((a, b) => b.failureRate - a.failureRate || a.stage - b.stage)
+    .map(item => item.stage);
+};
